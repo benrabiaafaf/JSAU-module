ded5c0f3017ec706778143db8c7f96deaed6b1be
abe <afef@example.com>
2019-11-21 14:12:19 +0100

    nothing special

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const news_categories = ['Breaking News','Politics','Sports','Technology','Legal','Lifestyle','Art','Economical','Fashion']
+
+const checkAtributes = () =>{
+  
+}
+const validate = () => {
+
+}
+
+export { news_categories, validate }
