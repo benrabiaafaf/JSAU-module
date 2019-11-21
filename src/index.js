df918754cc5b6ed82596df41af91a58d99286fba
abe <afef@example.com>
2019-11-21 15:32:19 +0100

    package config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const news_categories = ['Breaking News','Politics','Sports','Technology','Legal','Lifestyle','Art','Economical','Fashion']
 
 const checkAtributes = () =>{
-  
+
 }
 const validate = () => {
 
