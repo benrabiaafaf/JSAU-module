ccb959400aef4b1365b57e4a4f6e51a845682109
abe <afef@example.com>
2019-11-21 16:22:01 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const news_categories = ['Breaking News','Politics','Sports','Technology','Legal','Lifestyle','Art','Economical','Fashion']
 
-const checkAtributes = () =>{
+const checkAtributes = (data) =>{
 
 }
+
 const validate = () => {
 
 }
 
 export { news_categories, validate }
+
+// const Modules = { news_categories, validate }
+// module.exports = Modules
